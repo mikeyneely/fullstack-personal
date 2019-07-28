46e30962284ab0349030789009cdcadd43746b7d
Mikey Neely <mike.neely90@gmail.com>
2019-07-28 19:43:28 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://sirmikeyrocks:meleah22@crud1-68o08.mongodb.net/gameSite?retryWrites=true&w=majority',
+    'dbName': 'demo'
+};
